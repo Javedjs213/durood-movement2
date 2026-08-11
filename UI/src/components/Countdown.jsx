e99132b7e961b4401b8d905333a04c20eda14cea
Javed Ansari <javedjs213@gmail.com>
2026-08-11 22:55:05 +0530

    Change countdown target date to September 25

--- UI/src/components/Countdown.jsx
+++ UI/src/components/Countdown.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 const Countdown = ({language}) => {
   const calculateTimeLeft = () => {
     const now = new Date();
-    const target = new Date(now.getFullYear(), 8, 5);
+    const target = new Date(now.getFullYear(), 8, 25);
 
     
     if (now > target) {
